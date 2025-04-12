656c8de42cd0f93ce2b4779418951d8a234fa5a0
HugoTheBugs <hugoferreira2323@gmail.com>
2025-04-12 15:09:08 -0300

    fix: ajustado a ordem dos atributos

--- src/main/java/Fema/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/Fema/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -28,11 +28,11 @@ public class NinjaModel {
     @Column (name = "img_url")
     private String imgUrl;
 
-    @Column (name = "rank")
-    private String rank;
-
     @Column (name = "idade")
     private int idade;
+
+    @Column (name = "rank")
+    private String rank;
 
     // @ManyToOne um ninja tem uma única missão!
     @ManyToOne
